ef647d8e48f3e01ba93603dbf2e0a5f747a34648
AhmadRazaJamal <ahmad.raza@me.com>
2023-01-15 16:12:09 -0700

    added a section to the left

--- src/Section.tsx
+++ src/Section.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import { Paper } from '@mui/material'
 
 const Section = () => {
+
     return (
         <div>
-            <Paper id='side-bar-01'>Hello</Paper>
+            <Paper id='side-bar-01' className="first-section section left">
+                <div className="progress-wrapper progress-bar-wrapper-left">
+                    <div className="progress-bar"></div>
+                </div>
+
+                <div className="section-intro-wrapper">
+                    <h1 className="section-title">
+                        <span className="section-title-text">About Me</span>
+                        <div className="section-title-decoration styleOne"></div>
+                        <div className="section-title-decoration styleTwo"></div>
+                        <div className="section-title-decoration styleThree"></div>
+                    </h1>
+                    <span className="section-number">01</span>
+                </div>
+
+
+                <div className="section-detail-wrapper">
+
+                    <p className="section-text">Hi there 👋! I'm a third-year digital media student from UK currently studying in Germany. My dream is to work for Disney or Pixar one day.</p>
+                    <p className="section-text"> I love creating art and playing with my cats! I also like drinking bubble tea and going for hikes! Totally hippie lol ✌️. Welcome to my portfolio!</p>
+                </div>
+            </Paper>
             <h1 style={{ top: '140vh', left: '20vh', position: 'absolute' }}>Regular overlay Text</h1>
             <h1 style={{ top: '240vh', left: '20vh', position: 'absolute' }}>Regular overlay Text</h1>
         </div>
