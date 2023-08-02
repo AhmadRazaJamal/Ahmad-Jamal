935fe057d3eef99cd1d09763fe445f39648c3a62
AhmadRazaJamal <ahmad.raza@me.com>
2023-08-01 21:26:12 -0600

    added icons instead of text

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useRef } from 'react';
 import anime from 'animejs';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 function Intro(): JSX.Element {
-  const textRef = useRef<HTMLHeadingElement>(null);
+  const iconRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
     setTimeout(() => {
-      const textWrapper = textRef.current?.querySelector('.letters');
-      if (textWrapper) {
-        textWrapper.innerHTML = textWrapper.textContent?.replace(/\S/g, "<span class='letter'>$&</span>") || '';
-      }
-
       anime.timeline({ loop: false })
         .add({
-          targets: '.ml6 .letter',
+          targets: iconRef.current!,
           translateY: ["1.1em", 0],
-          translateZ: 0,
-          duration: 1500,
-          delay: (el, i) => 50 * i
+          opacity: [0, 1],
+          duration: 1000,
+          delay: 1500,
+          begin: () => {
+            iconRef.current!.style.display = 'inline-block';
+          }
         });
     }, 5000);  // Delay of 5 seconds
   }, []);
@@ -25,9 +24,13 @@ function Intro(): JSX.Element {
 
   return (
     <div className="center-screen">
-      <h1 className="ml6" ref={textRef}>
+      <h1 className="ml6">
         <span className="text-wrapper">
-          <span className="letters">WELCOME TO MY OFFICE</span><br />
+          <KeyboardArrowUpIcon
+            ref={iconRef}
+            fontSize="large"
+            style={{ width: 24, position: 'relative', top: 8, left: 4, strokeWidth: 8 }}
+          />
         </span>
       </h1>
     </div>
--- src/Sections.tsx
+++ src/Sections.tsx
@@ -67,9 +67,9 @@ export const Sections = () => {
 
                 <div className="section-detail-wrapper">
 
-                    <p className="section-text">Hello internet <span className="wave">👋</span> ! I'm Ahmad Jamal, but you can call me Jamal. I'm a full-stack developer based in Calgary, and I'm currently working remotely for the awesome travel company FreshTracks, situated in Vancouver.</p>
+                    <p className="section-text">Hello internet <span className="wave">👋</span> ! I'm Ahmad Jamal, but you can call me Jamal. I'm a full-stack developer based in Calgary, and I'm currently working remotely for the awesome travel company FreshTracks, located in Vancouver.</p>
                     <p className="section-text">Having worked professionally as a developer for almost 3 years, I've had the pleasure of collaborating with some exciting startups, including HigherGround, FreshGrade, and Freshtracks. These experiences have allowed me to hone my development skills and gain valuable insights into the industry.</p>
-                    <p className="section-text">I'm a big believer in always pushing myself to learn new things, which is why I've poured my heart and soul into the revamp of my old portfolio. I've used some fantastic tools like NextJS, tailwind, and threeJS to make it stand out visually and functionally.</p>
+                    <p className="section-text">I'm a big believer in always pushing myself to learn new things, which is why I've poured my heart and soul into the revamp of my old portfolio. I've used some fantastic tools like NextJS, Tailwind, and ThreeJS to make it stand out visually and functionally.</p>
                     <p className="section-text">So, dive in and have fun exploring my office!</p>
                 </div>
             </div>
@@ -104,7 +104,6 @@ export const Sections = () => {
                             <p />
                             Here is my complete research and thought process of how I made this possible: <a href={'https://wandering-psychology-b1a.notion.site/SEO-Optimization-Analysis-41a6620719af46ee9be5636e88f38d15?pvs=4'}>Link to my Notion webpage</a>
                         </p>
-                        <ImageGallery items={laurieKossProjectImages} showNav={false} showFullscreenButton={false} slideInterval={2000} />
                     </div>
 
                 </section>
@@ -117,16 +116,66 @@ export const Sections = () => {
 
                 <div className="section-intro-wrapper">
                     <h1 className="section-title">
-                        <span className="section-title-text">Skills</span>
+                        <span className="section-title-text">What can I do for you?</span>
                     </h1>
-                    <span className="section-number">01</span>
+                    <span className="section-number">03</span>
                 </div>
 
 
                 <div className="section-detail-wrapper">
-
-                    <p className="section-text">Hi there 👋! I'm a third-year digital media student from UK currently studying in Germany. My dream is to work for Disney or Pixar one day.</p>
-                    <p className="section-text"> I love creating art and playing with my cats! I also like drinking bubble tea and going for hikes! Totally hippie lol ✌️. Welcome to my portfolio!</p>
+                    <h2 className="section-heading">1. Web Development</h2>
+                    <p className="section-text"></p>
+                    <ul className="section-text">
+                        <li>
+                            Full-Stack Wizard: From the front end to the back end, I conjure up a complete web environment.
+                        </li>
+                        <li>
+                            Your site's personal health guru: I'm all about keeping things updated, fixed, and running smoothly.
+                        </li>
+                    </ul>
+                    <h2 className="section-heading">2. Search Engine Optimization</h2>
+                    <p className="section-text"></p>
+                    <ul className="section-text">
+                        <li>
+                            Improving Core Web Vitals for your website, so you can rank higher.
+                        </li>
+                        <li>
+                            Google Analytics and Search Console: I don't just set it up, but act as your guide in understanding data.
+                        </li>
+                        <li>
+                            Providing guidance on areas where you can improve your Google rankings.
+                        </li>
+                    </ul>
+                    <h2 className="section-heading">3. CMS Management</h2>
+                    <p className="section-text"></p>
+                    <ul className="section-text">
+                        <li>
+                            Setting up, updating, and maintaining your CMS. Be it headless or not.
+                        </li>
+                        <li>
+                            Ensuring your project's plugins are always on their latest and greatest form.
+                        </li>
+                    </ul>
+                    <h2 className="section-heading">4. Continuous Integration/Deployment</h2>
+                    <p className="section-text"></p>
+                    <ul className="section-text">
+                        <li>
+                            Automating, testing and deploying to make your life easier and your code robust.
+                        </li>
+                        <li>
+                            Cut down on coffee breaks waiting for code deployment. Let's make it swift and reliable.
+                        </li>
+                    </ul>
+                    <h2 className="section-heading">5. Solution Architect</h2>
+                    <p className="section-text"></p>
+                    <ul className="section-text">
+                        <li>
+                            Designing solutions for tough problems.
+                        </li>
+                        <li>
+                            Research, test and implement best way to solve problems keeping you up at night.
+                        </li>
+                    </ul>
                 </div>
             </div>
 
@@ -147,16 +196,28 @@ export const Sections = () => {
                     </div>
 
                     <div className="section-detail-wrapper">
-                        <h3 className="section-heading">Candycane Village</h3>
-                        <p className="section-text">This project is in progress but it's about a super colorful village where the entire world including the people are candies. So far the story is that they are set out to explore their "space" only to realize it's a human that will try to destroy them.</p>
-                        <h3 className="section-heading">Rebecca's Reddish Radishes</h3>
-                        <p className="section-text">Oh what's that? Why, it's a red radish! Oop, another one! In this playful and comedy animation, Rebecca, a young farmer, decided to plant radishes for the first time, but there is a big twist!</p>
-                        <h3 className="section-heading">Flora</h3>
-                        <p className="section-text">A heartwarming story about a little orphan girl who tries to find her way back home.</p>
+                        <h3 className="section-heading">FreshTracks</h3>
+                        <p className="section-text">FreshTracks is where I am currently working! (unless it's been years since my last update yikes!).</p>
+                        <p className="section-text">It's a Vancouver-based company that has been creating personalized Canadian vacations since 1996. My role here is that of a full stack developer. I have, so far, completed the creation and deployment of 3 full scale websites:</p>
+                        <ul className="section-text">
+                            <li><a href="https://freshtrackscanada.com/">Fresh Tracks Canada</a></li>
+                            <li><a href="https://canadiantrainvacations.com/">Canadian Train Vacations</a></li>
+                            <li><a href="https://northernlightscanada.com/">Northern Lights Canada</a></li>
+                        </ul>
+                        <p className="section-text">These websites were developed using the latest NextJS framework, utilizing the headless CMS model with DatoCMS for content management.</p>
+                        <h3 className="section-heading">FreshGrade </h3>
+                        <p className="section-text">At FreshGrade, an ed-tech startup, I created student portfolios on our web-based platform with an interactive feed for communication and cross-collaboration between parents, students, and teachers. I developed user-friendly features to enhance
+                            the product's functionality while meeting the needs of educators and students.
+                        </p>
+                        <p className="section-text">This experience allowed me to gain a deep understanding of the intersection between education and technology, and hone my skills in product development, user experience, and collaboration.</p>
+                        <h3 className="section-heading">Higher Ground</h3>
+                        <p className="section-text">FreshGrade was later acquired by HigherGround which is a Montessori-inspired educational offering. During my time at HigherGround, I worked on improving the scalability and website performance of the existing infrastructure. I also collaborated with a team of designers and developers to create new features and functionalities for the product. All of this allowed me to further enhance my skills in software engineering, teamwork, and project management.</p>
+
+                        <p className="section-text">For more information about what I do and where I’ve worked, please check out my <a href="https://www.linkedin.com/in/ahmadjamal/">LinkedIn</a></p>
                     </div>
 
                 </section>
-            </div>
+            </div >
 
             <div id='side-bar-05' className="first-section section-left left">
                 <div className="progress-wrapper progress-bar-wrapper-left">
@@ -167,7 +228,7 @@ export const Sections = () => {
                     <h1 className="section-title">
                         <span className="section-title-text">Get in touch</span>
                     </h1>
-                    <span className="section-number">01</span>
+                    <span className="section-number">05</span>
                 </div>
 
 
