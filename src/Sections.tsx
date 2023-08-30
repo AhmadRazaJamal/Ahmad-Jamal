a5a1067e7a01ac0dc2be0868a4770cdde8175590
AhmadRazaJamal <ahmad.raza@me.com>
2023-08-29 21:52:19 -0600

    copy changes

--- src/Sections.tsx
+++ src/Sections.tsx
@@ -68,7 +68,7 @@ export const Sections = () => {
                 <div className="section-detail-wrapper">
 
                     <p className="section-text">Hello internet <span className="wave">👋!</span> My name is Ahmad Jamal, but you can call me Jamal. I'm a Full-Stack Developer based in Calgary, and I'm currently working remotely for an awesome travel company FreshTracks, located in the heart of Vancouver downtown.</p>
-                    <p className="section-text">Having worked professionally as a developer for almost 3 years, I've had the pleasure of collaborating with some exciting startups, including HigherGround, FreshGrade, and Freshtracks. These experiences have allowed me to hone my development skills and gain valuable insights into the industry.</p>
+                    <p className="section-text">Having worked professionally as a developer for almost 4 years, I've had the pleasure of collaborating with some exciting startups, including HigherGround, FreshGrade, and Fresh Tracks Canada. These experiences have allowed me to hone my development skills and gain valuable insights into the industry.</p>
                     <p className="section-text">I'm a big believer in always pushing myself to learn new things, which is why I've poured my heart and soul into the revamp of my old portfolio. I've used some fantastic tools like NextJS, Tailwind, and ThreeJS to make it stand out visually and functionally.</p>
                     <p className="section-text">So, dive in and have fun exploring my office!</p>
                 </div>
@@ -91,7 +91,7 @@ export const Sections = () => {
 
                     <div className="section-detail-wrapper">
                         <h2 className="section-heading">ThreeJS Projects</h2>
-                        <p className="section-text">My latest endeavor has been learning ThreeJS, and creating fun small projects while following this amazing <a href={'https://threejs-journey.com/'}>course by Bruno Simons</a>. I have worked on both small and large-scale projects such as my portfolio that utilizes ThreeJS to the fullest. My portfolio goes beyond just using vanilla ThreeJS but uses React Three Fiber and includes a mini version of my home office designed in Blender.</p>
+                        <p className="section-text">My latest endeavour has been learning ThreeJS, and creating fun small projects while following this amazing <a href={'https://threejs-journey.com/'}>course by Bruno Simons</a>. I have worked on both small and large-scale projects such as my portfolio that utilizes ThreeJS to the fullest. My portfolio goes beyond just using vanilla ThreeJS but uses React Three Fiber and includes a mini version of my home office designed in Blender.</p>
                         <CarouselGallery content={threeJsProjectContent} />
 
                         <h2 className="section-heading">Canadian Train Vacations</h2>
@@ -192,52 +192,32 @@ export const Sections = () => {
                         <h1 className="section-title blue-text blue-border">
                             <span className="section-title-text blue-text">Experience</span>
                         </h1>
-                        <span className="section-number blue-text">02</span>
+                        <span className="section-number blue-text">04</span>
                     </div>
 
                     <div className="section-detail-wrapper">
                         <h3 className="section-heading">FreshTracks</h3>
                         <p className="section-text">FreshTracks is where I am currently working! (unless it's been years since my last update yikes!).</p>
-                        <p className="section-text">It's a Vancouver-based company that has been creating personalized Canadian vacations since 1996. My role here is that of a full stack developer. I have, so far, completed the creation and deployment of 3 full scale websites:</p>
+                        <p className="section-text">It's a Vancouver-based company that has been creating personalized Canadian vacations since 1996. My role here is that of a Full Stack Developer. I have, so far, completed the creation and deployment of 3 full scale websites:</p>
                         <ul className="section-text">
-                            <li><a href="https://freshtrackscanada.com/">Fresh Tracks Canada</a></li>
-                            <li><a href="https://canadiantrainvacations.com/">Canadian Train Vacations</a></li>
-                            <li><a href="https://northernlightscanada.com/">Northern Lights Canada</a></li>
+                            <li><a href="https://freshtrackscanada.com/" target="_blank">Fresh Tracks Canada</a></li>
+                            <li><a href="https://canadiantrainvacations.com/" target="_blank">Canadian Train Vacations</a></li>
+                            <li><a href="https://northernlightscanada.com/" target="_blank">Northern Lights Canada</a></li>
                         </ul>
-                        <p className="section-text">These websites were developed using the latest NextJS framework, utilizing the headless CMS model with DatoCMS for content management.</p>
+                        <p className="section-text">These websites were developed using NextJS, utilizing a headless CMS model for content management along with a CI/CD pipeline to automate deployments</p>
                         <h3 className="section-heading">FreshGrade </h3>
                         <p className="section-text">At FreshGrade, an ed-tech startup, I created student portfolios on our web-based platform with an interactive feed for communication and cross-collaboration between parents, students, and teachers. I developed user-friendly features to enhance
                             the product's functionality while meeting the needs of educators and students.
                         </p>
-                        <p className="section-text">This experience allowed me to gain a deep understanding of the intersection between education and technology, and hone my skills in product development, user experience, and collaboration.</p>
+                        <p className="section-text">This experience allowed me to gain a deep understanding of the intersection between education and technology, and to hone my skills in product development, user experience, and collaboration.</p>
                         <h3 className="section-heading">Higher Ground</h3>
                         <p className="section-text">FreshGrade was later acquired by HigherGround which is a Montessori-inspired educational offering. During my time at HigherGround, I worked on improving the scalability and website performance of the existing infrastructure. I also collaborated with a team of designers and developers to create new features and functionalities for the product. All of this allowed me to further enhance my skills in software engineering, teamwork, and project management.</p>
 
-                        <p className="section-text">For more information about what I do and where I’ve worked, please check out my <a href="https://www.linkedin.com/in/ahmadjamal/">LinkedIn</a></p>
+                        <p className="section-text">For further information about what I do and where I’ve worked, please check out my <a href="https://www.linkedin.com/in/ahmadjamal/">LinkedIn</a></p>
                     </div>
 
                 </section>
             </div >
-
-            <div id='side-bar-05' className="first-section section-left left">
-                <div className="progress-wrapper section-one-progress-bar">
-                    <div className="progress-bar"></div>
-                </div>
-
-                <div className="section-intro-wrapper">
-                    <h1 className="section-title">
-                        <span className="section-title-text">Get in touch</span>
-                    </h1>
-                    <span className="section-number">05</span>
-                </div>
-
-
-                <div className="section-detail-wrapper">
-
-                    <p className="section-text">Hi there 👋! I'm a third-year digital media student from UK currently studying in Germany. My dream is to work for Disney or Pixar one day.</p>
-                    <p className="section-text"> I love creating art and playing with my cats! I also like drinking bubble tea and going for hikes! Totally hippie lol ✌️. Welcome to my portfolio!</p>
-                </div>
-            </div>
         </div >
     )
 }
