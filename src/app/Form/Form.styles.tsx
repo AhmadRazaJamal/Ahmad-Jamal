2be3b3674be7eac7d2d45e5142c5aa4437f4ec65
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-06-11 15:22:21 -0600

    fixing mobile form styles

--- src/app/Form/Form.styles.tsx
+++ src/app/Form/Form.styles.tsx
@@ -8,12 +8,6 @@ export const StyledForm = styled.form`
 
 export const FormWrapper = styled.div`
   display: flex;
-  background: white;
-  border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5px);
-  border: 1px solid rgba(255, 255, 255, 0.3);
-  padding: 24px;
   height: 424px;
 
   input{
