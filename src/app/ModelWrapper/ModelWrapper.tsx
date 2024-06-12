a3c2de174d22e0944d9557d6adbe90f9e7e0e12b
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-06-12 13:29:31 -0600

    fixing section border

--- src/app/ModelWrapper/ModelWrapper.tsx
+++ src/app/ModelWrapper/ModelWrapper.tsx
@@ -131,11 +131,11 @@ const Office: React.FC<OfficeProps> = ({ model, scale, isInteractiveMode }) => {
       }
 
       animateSectionBorders('section-one', scroll.offset, 300, 0, 0, 300, 0.1, 0.14, 0.172, 0.22);
-      animateSectionBorders('section-two', scroll.offset, 300, 0, 0, 250, 0.35, 0.5, 0.56, 0.57);
+      animateSectionBorders('section-two', scroll.offset, 300, 0, 0, 250, 0.33, 0.37, 0.535, 0.6);
       animateSectionBorders('section-three', scroll.offset, 300, 0, 0, 250, 0.7, 0.74, 0.82, 0.85);
 
       changeProgressBarHeight('progress-bar-one', scroll.offset, 0.12, scroll.offset > 0.12 && scroll.offset < 0.24, 4000, 'rgb(70, 130, 180, 0.5)');
-      changeProgressBarHeight('progress-bar-two', scroll.offset, 0.36, scroll.offset > 0.36 && scroll.offset < 0.6, 2830, 'rgb(253, 216, 53, 0.5)');
+      changeProgressBarHeight('progress-bar-two', scroll.offset, 0.33, scroll.offset > 0.33 && scroll.offset < 0.55, 2980, 'rgb(253, 216, 53, 0.5)');
       changeProgressBarHeight('progress-bar-three', scroll.offset, 0.7, scroll.offset > 0.7 && scroll.offset < 0.85, 3200, 'rgb(57, 150, 122, 0.8)');
     }
   });
