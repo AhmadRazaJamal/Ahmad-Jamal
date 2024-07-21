9a20f0ddc615bd393c269f55a5da3b99dcde9c61
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-07-21 10:44:46 -0600

    updated the threejs videos and corrected mobile height

--- src/app/VideoPlayer/VideoPlayer.styles.tsx
+++ src/app/VideoPlayer/VideoPlayer.styles.tsx
@@ -8,20 +8,46 @@ export const PlayerWrapper = styled.div`
 `;
 
 export const VideoPlayerCustom = styled.div`
-  padding-top: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  .react-player-0 {
+    height: 170px !important;
+  }
 
-  h3 {
+  .react-player-1 {
+    height: 170px !important;
+  }
+
+  div:nth-child(4) {
+    padding-top: 100px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+  button {
+    width: 1.25rem;
+    padding: 1px;
+    }
+  }
+
+  h3{
     font-weight: 100;
     font-size: 18px;
-    margin: 36px 0 0;
+    margin-bottom: 0;
+    margin-top: 36px;
   }
 
   p {
     color: gray;
     font-size: 14px;
-    margin-bottom: 24px;
+    margin-bottom:24px;
+  }
+
+  @media (min-width: 768px) {
+    .react-player-0 {
+      height: 300px !important; 
+    }
+
+    .react-player-1 {
+      height: 300px !important;
+    }
   }
 `;
