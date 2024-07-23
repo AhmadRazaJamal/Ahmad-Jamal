4dbf774770f0e70745f04889e3542c77b721e1b6
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-07-22 21:34:37 -0600

    clean up of constants file

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,7 +1,5 @@
 export const isMobileScreen = typeof window !== 'undefined' && window.innerWidth <= 425;
 export const isSmallScreen = typeof window !== 'undefined' && window.innerWidth <= 768;
-export const isMediumScreen = typeof window !== 'undefined' && window.innerWidth <= 1024;
-export const isLargeScreen = typeof window !== 'undefined' && window.innerWidth <= 1440;
-export const isExtraLargeScreen = typeof window !== 'undefined' && window.innerWidth >= 1440;
+
 export const isMobileDevice = () => /Mobi|Android/i.test(navigator.userAgent);
 export const isSafariBrowser = () => /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
