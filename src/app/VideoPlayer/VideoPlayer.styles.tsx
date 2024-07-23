30ed0bde396297ff94480e5730e5cf60882169cb
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-07-22 22:06:01 -0600

    refactored the VideoPlayer component

--- src/app/VideoPlayer/VideoPlayer.styles.tsx
+++ src/app/VideoPlayer/VideoPlayer.styles.tsx
@@ -5,13 +5,11 @@ export const ReactPlayerStyled = styled.div`
 `;
 
 export const PlayerWrapper = styled.div`
+  /* No specific styles needed for this wrapper */
 `;
 
 export const VideoPlayerCustom = styled.div`
-  .react-player-0 {
-    height: 170px !important;
-  }
-
+  .react-player-0,
   .react-player-1 {
     height: 170px !important;
   }
@@ -22,13 +20,13 @@ export const VideoPlayerCustom = styled.div`
     flex-direction: row;
     justify-content: center;
 
-  button {
-    width: 1.25rem;
-    padding: 1px;
+    button {
+      width: 1.25rem;
+      padding: 1px;
     }
   }
 
-  h3{
+  h3 {
     font-weight: 100;
     font-size: 18px;
     margin-bottom: 0;
@@ -38,14 +36,11 @@ export const VideoPlayerCustom = styled.div`
   p {
     color: gray;
     font-size: 14px;
-    margin-bottom:24px;
+    margin-bottom: 24px;
   }
 
   @media (min-width: 768px) {
-    .react-player-0 {
-      height: 300px !important; 
-    }
-
+    .react-player-0,
     .react-player-1 {
       height: 300px !important;
     }
