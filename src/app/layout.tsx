b65c9864a6acd4d5c422b52a7a579ed4513bd89d
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2024-07-23 20:36:58 -0600

    changed the description meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jamal's Portfolio",
-  description: "Explore the portfolio of Ahmad Jamal, a Full-Stack Developer from Calgary. With nearly 20 years in tech and 4 years of professional experience, Ahmad has worked with startups like HigherGround and FreshGrade. Discover his projects using modern tools like Next.js, Tailwind, Three.js, and Blender.",
+  title: "Ahmad Jamal's Portfolio",
+  description: "Hello from Calgary, I am a Full-Stack Developer with nearly 5 years of professional experience. I have worked with startups like HigherGround FreshGrade and FreshTracks. Explore my portfolio and learn more about what I love to do",
 };
 
 export default function RootLayout({
@@ -19,20 +19,20 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="floralwhite" />
-        <meta name="description" content="Explore the portfolio of Ahmad Jamal, a Full-Stack Developer from Calgary. With nearly 20 years in tech and 4 years of professional experience, Ahmad has worked with startups like HigherGround and FreshGrade. Discover his projects using modern tools like Next.js, Tailwind, Three.js, and Blender." />
+        <meta name="description" content="Hello from Calgary, I am a Full-Stack Developer with nearly 5 years of professional experience. I have worked with startups like HigherGround FreshGrade and FreshTracks. Explore my portfolio and learn more about what I love to do" />
         <meta name="keywords" content="Ahmad Jamal, Full-Stack Developer, Web Developer, Portfolio, Calgary, HigherGround, FreshGrade, Fresh Tracks Canada, Next.js, Tailwind, Three.js, Blender, CI/CD, SEO, Core Web Vitals, Google Analytics, Search Console, A/B Testing, Personalization, Solution Architect, CMS Management, Galaxy Generator, Canadian, Canada, Alberta" />
         <meta name="author" content="Ahmad J." />
         <meta property="og:title" content="Ahmad Jamal's Portfolio" />
-        <meta property="og:description" content="Explore the portfolio of Ahmad Jamal, a Full-Stack Developer from Calgary. With nearly 20 years in tech and 4 years of professional experience, Ahmad has worked with startups like HigherGround and FreshGrade. Discover his projects using modern tools like Next.js, Tailwind, Three.js, and Blender." />
+        <meta property="og:description" content="Hello from Calgary, I am a Full-Stack Developer with nearly 5 years of professional experience. I have worked with startups like HigherGround FreshGrade and FreshTracks. Explore my portfolio and learn more about what I love to do" />
         <meta property="og:image" content="/media/og-image.png" />
         <meta property="og:url" content="https://ahmadrjamal.com" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Ahmad Jamal's Portfolio" />
-        <meta name="twitter:description" content="Explore the portfolio of Ahmad Jamal, a Full-Stack Developer from Calgary. With nearly 20 years in tech and 4 years of professional experience, Ahmad has worked with startups like HigherGround and FreshGrade. Discover his projects using modern tools like Next.js, Tailwind, Three.js, and Blender." />
+        <meta name="twitter:description" content="Hello from Calgary, I am a Full-Stack Developer with nearly 5 years of professional experience. I have worked with startups like HigherGround FreshGrade and FreshTracks. Explore my portfolio and learn more about what I love to do" />
         <meta name="twitter:image" content="/media/og-image.png" />
         <link rel="icon" href="/favicon.ico" />
-        <title>Ahmad R Jamal Portfolio</title>
+        <title>Ahmad R. Jamal Portfolio</title>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
